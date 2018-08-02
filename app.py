79d9a33ee03d4d699659d01da144585ec112041b
Eshno <oliver-felix@hotmail.com>
2018-08-02 02:34:28 -0400

    Agregado App.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from Tkinter import *
+from tkFileDialog import *
+
+class App:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Editor de Texto Python")
+        self.root.minsize(width=400,height=400)
+        self.root.maxsize(width=400,height=400)
+    
+
